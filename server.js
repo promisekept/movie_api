6c0f2ead74a66e9b7f40f411e623efff03780cff
H A <promisekept836@outlook.com>
2021-07-26 23:41:03 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+// import * as http from "http";
+const http = require("http"),
+  url = require("url"),
+  fs = require("fs");
+http
+  .createServer((req, res) => {
+    let adr = req.url;
+    let parsedAdr = url.parse(adr, true);
+    filepath = "";
+    if (parsedAdr.pathname.includes("documentation")) {
+      filepath = `${__dirname}/documentation.html`;
+    } else {
+      filepath = "index.html";
+    }
+    if (!adr.includes("server") && !adr.includes("favicon")) {
+      fs.appendFile(
+        "log.txt",
+        `URL: ${adr}
+         Timestamp: ${new Date()}
+        
+        `,
+        function (err) {
+          if (err) {
+            throw err;
+          }
+        }
+      );
+    }
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  })
+  .listen(8080);
+console.log(`The server is up and running`);
+
+//Is filepath is global variable?  Why was I able to use filepath with lowercase p?
+//Why didn't it force me use let or const?
+
+//could not use import, had to use require
+//url.parse method is depreciated
+
+//The template literal does not work the way it's supposed to
