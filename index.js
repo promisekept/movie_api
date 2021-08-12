796ece95c45a8db9351a1301b5ca506b4c62e416
H A <promisekept836@outlook.com>
2021-08-12 01:44:50 -0400

    added express methods and updated documentation

--- index.js
+++ index.js
@@ -1,36 +1,89 @@
-const express = require("express"),
-  morgan = require("morgan");
+const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
-app.use(morgan("common"));
-
-const top10 = [
-  { title: "Old" },
-  { title: "Space Jam" },
-  { title: "Snake Eyes" },
-  { title: "Black Widow" },
-  { title: "Pig" },
-  { title: "Dead pigs" },
-  { title: "Gunpowder Milkshake" },
-  { title: "Blood Red Sky" },
-  { title: "The Tomorrow War" },
-  { title: "Fear Street Part One: 1994" },
+const movies = [
+  {
+    title: "Gunpowder Milkshake",
+    description: "",
+    genre: "action",
+    director: "Navot Papushado",
+    imageURL: "",
+    featured: true,
+  },
+  {
+    title: "Blood Red Sky",
+    description: "",
+    genre: "comedy",
+    director: "Director 2",
+    imageURL: "",
+    featured: false,
+  },
+  {
+    title: "Jungle Cruise",
+    description: "",
+    genre: "horror",
+    director: "Director 3",
+    imageURL: "",
+    featured: true,
+  },
+  {
+    title: "Jolt",
+    description: "",
+    genre: "thriller",
+    director: "Director 4",
+    imageURL: "",
+    featured: false,
+  },
+  {
+    title: "Dune",
+    description: "",
+    genre: "romance",
+    director: "Director 5",
+    imageURL: "",
+    featured: true,
+  },
 ];
 
 app.get("/movies", (req, res) => {
-  res.json(top10);
+  res.json(movies);
+  // res.send("Successful GET request, returning data on all the movies");
 });
 
-app.get("/", (req, res) => {
-  res.send("This is my default page");
+app.get("/movies/:title", (req, res) => {
+  res.json(
+    movies.find((movie) => {
+      return movie.title === req.params.title;
+    })
+  );
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.sendStatus(500);
+app.get("/genres/:NameOrTitle", (req, res) => {
+  res.send(`Here's the name or title: ${req.params.NameOrTitle}`);
 });
 
-app.listen(2021, () => {
-  console.log("Your app is listening on port 2021");
+app.get("/directors/:Name", (req, res) => {
+  res.send(`Here's the director's name: ${req.params.Name}`);
 });
+
+app.post("/users", (req, res) => {
+  res.send(`Successful POST request to register a a new user`);
+});
+
+app.put("/users/:Username", (req, res) => {
+  res.send(`Successful PUT request to update user info`);
+});
+
+app.post("/users/:user/favorites/:Movie", (req, res) => {
+  res.send(
+    `Successful POST request to add a movie to a user's list of favorites`
+  );
+});
+
+app.delete("/users/:RemoveMovie", (req, res) => {
+  res.send(`Successful DELETE request to remove a user's movie`);
+});
+
+app.delete("/users:Username", (req, res) => {
+  res.send(`Successful DELETE request to remove a user`);
+});
+app.listen(5500, () => console.log("The server is up and running"));
