0781f4f424148142651a86b449f48341ea3977f2
H A <promisekept836@outlook.com>
2021-11-24 22:18:56 -0500

    testing user endpoints

--- index.js
+++ index.js
@@ -80,10 +80,65 @@ app.get("/directors/:Name", (req, res) => {
   res.send(`Here's the director's name: ${req.params.Name}`);
 });
 
+//Add a user
+/* We'll expect JSON in this format
+{
+   ID: Integer,
+   Username: String,
+   Password: String,
+   Email: String,
+   Birthday: Date
+}*/
 app.post("/users", (req, res) => {
-  res.send(`Successful POST request to register a a new user`);
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + "already exists");
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
+//Get all users
+app.get("/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
+//Get a user by username
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 app.put("/users/:Username", (req, res) => {
   res.send(`Successful PUT request to update user info`);
 });
