cb736030c86105583653c9a272c2c7ef40cc05f7
H A <promisekept836@outlook.com>
2021-12-09 18:54:55 -0500

    finished movies by title

--- index.js
+++ index.js
@@ -16,64 +16,41 @@ mongoose.connect("mongodb://localhost:27017/movies", {
   useUnifiedTopology: true,
 });
 
-const movies = [
-  {
-    title: "Gunpowder Milkshake",
-    description: "",
-    genre: "action",
-    director: "Navot Papushado",
-    imageURL: "",
-    featured: true,
-  },
-  {
-    title: "Blood Red Sky",
-    description: "",
-    genre: "comedy",
-    director: "Director 2",
-    imageURL: "",
-    featured: false,
-  },
-  {
-    title: "Jungle Cruise",
-    description: "",
-    genre: "horror",
-    director: "Director 3",
-    imageURL: "",
-    featured: true,
-  },
-  {
-    title: "Jolt",
-    description: "",
-    genre: "thriller",
-    director: "Director 4",
-    imageURL: "",
-    featured: false,
-  },
-  {
-    title: "Dune",
-    description: "",
-    genre: "romance",
-    director: "Director 5",
-    imageURL: "",
-    featured: true,
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("Welcome to MyFlix");
 });
 
 app.get("/movies", (req, res) => {
-  res.json(movies);
-  // res.send("Successful GET request, returning data on all the movies");
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error:" + err);
+    });
 });
 
-app.get("/movies/:title", (req, res) => {
-  res.json(
-    movies.find((movie) => {
-      return movie.title === req.params.title;
+// app.get("/users/:Username", (req, res) => {
+//   Users.findOne({ Username: req.params.Username })
+//     .then((user) => {
+//       res.json(user);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error: " + err);
+//     });
+// });
+
+app.get("/movies/:Title", (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.json(movie);
     })
-  );
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 app.get("/genres/:NameOrTitle", (req, res) => {
@@ -128,7 +105,7 @@ app.get("/users", (req, res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).send("Error: " + err + "it works");
+      res.status(500).send("Error: " + err);
     });
 });
 
